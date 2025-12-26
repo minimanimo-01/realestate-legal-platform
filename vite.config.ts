326dae775435abfce86bbe5afb80bfa422df5bd0
minimanimo-01 <kuminhee023@gmail.com>
2025-12-26 19:31:25 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
@@ -51,7 +52,7 @@ export default defineConfig({
     },
   },
   css: {
-    postcss: './postcss.config.js',  // ← 이 부분만 추가!
+    postcss: './postcss.config.js',
   },
   build: {
     target: 'esnext',
